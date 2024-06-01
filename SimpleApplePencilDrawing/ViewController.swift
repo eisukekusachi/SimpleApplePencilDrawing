b2727c68fb526b52ff6bb46ef26da394a91c9b66
EisukeKusachi <eisukekusachi@gmail.com>
2024-06-01 11:19:32 +0900

    Initial Commit

---
+++ SimpleApplePencilDrawing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleApplePencilDrawing
+//
+//  Created by Eisuke Kusachi on 2024/06/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
