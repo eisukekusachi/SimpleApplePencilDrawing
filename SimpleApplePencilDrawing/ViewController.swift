aba89e842e261477fb4aec8bd141489117a9334e
EisukeKusachi <eisukekusachi@gmail.com>
2024-08-23 22:52:20 +0900

    Rename `FingerInputGestureRecognizer`,`PencilInputGestureRecognizer` to `CanvasFingerInputGestureRecognizer`,`CanvasPencilInputGestureRecognizer`

--- SimpleApplePencilDrawing/ViewController.swift
+++ SimpleApplePencilDrawing/ViewController.swift
@@ -54,12 +54,12 @@ extension ViewController {
         // Remove `/* */` to enable finger drawing
         /*
         canvasView.addGestureRecognizer(
-            FingerInputGestureRecognizer(self)
+            CanvasFingerInputGestureRecognizer(self)
         )
         */
 
         canvasView.addGestureRecognizer(
-            PencilInputGestureRecognizer(self)
+            CanvasPencilInputGestureRecognizer(self)
         )
 
         // Add a gesture recognizer to clear the canvas when the screen is tapped with three fingers.
@@ -77,7 +77,7 @@ extension ViewController {
 
 }
 
-extension ViewController: FingerInputGestureSender {
+extension ViewController: CanvasFingerInputGestureSender {
 
     func sendFingerTouches(_ touches: Set<UITouch>, with event: UIEvent?, on view: UIView) {
         canvasViewModel.onFingerInputGesture(
@@ -89,7 +89,7 @@ extension ViewController: FingerInputGestureSender {
 
 }
 
-extension ViewController: PencilInputGestureSender {
+extension ViewController: CanvasPencilInputGestureSender {
 
     func sendPencilTouches(_ touches: Set<UITouch>, with event: UIEvent?, on view: UIView) {
         canvasViewModel.onPencilInputGesture(
