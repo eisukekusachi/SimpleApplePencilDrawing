09c18d5efc030939d156757f96b3dd5240d3845c
EisukeKusachi <eisukekusachi@gmail.com>
2024-10-05 20:08:49 +0900

    Add methods to create Bezier curve points in `CanvasGrayscaleDotPoint`

--- SimpleApplePencilDrawing/Views/CanvasView/Models/Drawing/CanvasGrayscaleDotPoint.swift
+++ SimpleApplePencilDrawing/Views/CanvasView/Models/Drawing/CanvasGrayscaleDotPoint.swift
@@ -29,6 +29,11 @@ extension CanvasGrayscaleDotPoint {
         self.brightness = touchPoint.maximumPossibleForce != 0 ? min(touchPoint.force, 1.0) : 1.0
         self.blurSize = blurSize
     }
+
+}
+
+extension CanvasGrayscaleDotPoint {
+    /// Calculate the average of two values
     static func average(_ left: Self, _ right: Self) -> Self {
         .init(
             location: left.location == right.location ? left.location : CGPoint(
@@ -36,8 +41,165 @@ extension CanvasGrayscaleDotPoint {
                 y: (left.location.y + right.location.y) * 0.5
             ),
             diameter: left.diameter == right.diameter ? left.diameter : (left.diameter + right.diameter) * 0.5,
-            brightness: left.brightness == right.brightness ? left.brightness : (left.brightness + right.brightness) * 0.5
+            brightness: left.brightness == right.brightness ? left.brightness : (left.brightness + right.brightness) * 0.5,
+            blurSize: left.blurSize == right.blurSize ? left.blurSize : (left.blurSize + right.blurSize) * 0.5
         )
+    }
+
+    static func makeCurvePoints(
+        from iterator: CanvasGrayscaleCurveIterator,
+        shouldIncludeFirstCurve: Bool,
+        shouldIncludeLastCurve: Bool
+    ) -> [Self] {
+        var array: [Self] = []
+
+        if shouldIncludeFirstCurve {
+            array.append(contentsOf: makeCurveFirstPoints(from: iterator))
+        }
+
+        array.append(contentsOf: makeCurveIntermediatePoints(from: iterator, shouldIncludeEndPoint: false))
+
+        if shouldIncludeLastCurve {
+            array.append(contentsOf: makeCurveFirstPoints(from: iterator))
+        }
+
+        return array
+    }
+
+    /// Make an array of first curve points from an iterator
+    static func makeCurveFirstPoints(
+        from iterator: CanvasGrayscaleCurveIterator
+    ) -> [Self] {
+        var curve: [Self] = []
+
+        if iterator.array.count >= 3,
+           let points = iterator.getBezierCurveFirstPoints() {
+
+            let bezierCurvePoints = BezierCurve.getFirstCurvePoints(
+                pointA: points.previousPoint.location,
+                pointB: points.startPoint.location,
+                pointC: points.endPoint.location,
+                addLastPoint: false
+            )
+            curve.append(
+                contentsOf: interpolateToMatchPointCount(
+                    targetPoints: bezierCurvePoints,
+                    interpolationStart: points.previousPoint,
+                    interpolationEnd: points.startPoint,
+                    shouldIncludeEndPoint: false
+                )
+            )
+        }
+        return curve
+    }
+
+    /// Make an array of curve points from an iterator with a range of 4 set
+    static func makeCurveIntermediatePoints(
+        from iterator: CanvasGrayscaleCurveIterator,
+        shouldIncludeEndPoint: Bool
+    ) -> [Self] {
+        var curve: [Self] = []
+
+        let pointArray = iterator.getBezierCurvePointsWithFixedRange4()
+
+        pointArray.enumerated().forEach { (index, points) in
+            let shouldIncludeEndPoint = index == pointArray.count - 1 ? shouldIncludeEndPoint : false
+
+            let bezierCurvePoints = BezierCurve.getCurvePoints(
+                previousPoint: points.previousPoint.location,
+                startPoint: points.startPoint.location,
+                endPoint: points.endPoint.location,
+                nextPoint: points.nextPoint.location,
+                addLastPoint: shouldIncludeEndPoint
+            )
+            curve.append(
+                contentsOf: interpolateToMatchPointCount(
+                    targetPoints: bezierCurvePoints,
+                    interpolationStart: points.startPoint,
+                    interpolationEnd: points.endPoint,
+                    shouldIncludeEndPoint: shouldIncludeEndPoint
+                )
+            )
+        }
+        return curve
+    }
+
+    /// Make an array of last curve points from an iterator
+    static func makeCurveLastPoints(
+        from iterator: CanvasGrayscaleCurveIterator
+    ) -> [Self] {
+        var curve: [Self] = []
+
+        if iterator.array.count >= 3,
+           let points = iterator.getBezierCurveLastPoints() {
+
+            let bezierCurvePoints = BezierCurve.getLastCurvePoints(
+                pointA: points.previousPoint.location,
+                pointB: points.startPoint.location,
+                pointC: points.endPoint.location,
+                addLastPoint: true
+            )
+            curve.append(
+                contentsOf: interpolateToMatchPointCount(
+                    targetPoints: bezierCurvePoints,
+                    interpolationStart: points.startPoint,
+                    interpolationEnd: points.endPoint,
+                    shouldIncludeEndPoint: true
+                )
+            )
+        }
+        return curve
+    }
+
+    /// Interpolate the values to match the number of elements in `targetPoints` array with that of the other elements array
+    static func interpolateToMatchPointCount(
+        targetPoints: [CGPoint],
+        interpolationStart: Self,
+        interpolationEnd: Self,
+        shouldIncludeEndPoint: Bool
+    ) -> [Self] {
+        var curve: [Self] = []
+
+        var numberOfInterpolations = targetPoints.count
+
+        if shouldIncludeEndPoint {
+            // Subtract 1 from `numberOfInterpolations` because the last point will be added to the arrays
+            numberOfInterpolations = numberOfInterpolations - 1
+        }
+
+        let brightnessArray = Interpolator.getLinearInterpolationValues(
+            begin: interpolationStart.brightness,
+            change: interpolationEnd.brightness,
+            duration: numberOfInterpolations,
+            addLastPoint: shouldIncludeEndPoint
+        )
+
+        let diameterArray = Interpolator.getLinearInterpolationValues(
+            begin: interpolationStart.diameter,
+            change: interpolationEnd.diameter,
+            duration: numberOfInterpolations,
+            addLastPoint: shouldIncludeEndPoint
+        )
+
+        let blurArray = Interpolator.getLinearInterpolationValues(
+            begin: interpolationStart.blurSize,
+            change: interpolationEnd.blurSize,
+            duration: numberOfInterpolations,
+            addLastPoint: shouldIncludeEndPoint
+        )
+
+        for i in 0 ..< targetPoints.count {
+            curve.append(
+                .init(
+                    location: targetPoints[i],
+                    diameter: diameterArray[i],
+                    brightness: brightnessArray[i],
+                    blurSize: blurArray[i]
+                )
+            )
+        }
+
+        return curve
     }
 
 }
---
+++ SimpleApplePencilDrawingTests/Views/CanvasView/Models/CanvasGrayscaleDotPointTests.swift
@@ -0,0 +1,359 @@
+//
+//  CanvasGrayscaleDotPointTests.swift
+//  SimpleApplePencilDrawingTests
+//
+//  Created by Eisuke Kusachi on 2024/10/05.
+//
+
+import XCTest
+@testable import SimpleApplePencilDrawing
+
+final class CanvasGrayscaleDotPointTests: XCTestCase {
+    /// Confirm that the average can be calculated
+    func testAverage() {
+        let result = CanvasGrayscaleDotPoint.average(
+            .generate(location: .init(x: 1, y: 1), diameter: 1, brightness: 1, blurSize: 1),
+            .generate(location: .init(x: 2, y: 2), diameter: 2, brightness: 2, blurSize: 2)
+        )
+        XCTAssertEqual(
+            result,
+            .generate(location: .init(x: 1.5, y: 1.5), diameter: 1.5, brightness: 1.5, blurSize: 1.5)
+        )
+    }
+
+    /// Confirm that the curve array can be created
+    func testMakeCurveIntermediatePoints() {
+        struct Condition {
+            let shouldIncludeEndPoint: Bool
+            let points: [CanvasGrayscaleDotPoint]
+        }
+        struct Expectation {
+            let result: [CanvasGrayscaleDotPoint]
+        }
+
+        let testCases: [(condition: Condition, expectation: Expectation)] = [
+            /// The case with fewer than 4 points
+            (
+                /// Confirm that an empty array is returned if there are fewer than four points, as four points are required to create an array of Bezier curve points.
+                condition: .init(
+                    shouldIncludeEndPoint: true,
+                    points: [
+                        .generate(location: .init(x: 0.5, y: 0.5), diameter: 1.0, brightness: 1.0),
+                        .generate(location: .init(x: 2.0, y: 2.0), diameter: 2.0, brightness: 2.0),
+                        .generate(location: .init(x: 3.5, y: 3.5), diameter: 3.0, brightness: 3.0)
+                    ]
+                ),
+                expectation: .init(
+                    result: []
+                )
+            ),
+            (
+                /// Confirm that an empty array is returned if there are fewer than four points, as four points are required to create an array of Bezier curve points.
+                condition: .init(
+                    shouldIncludeEndPoint: false,
+                    points: [
+                        .generate(location: .init(x: 0.5, y: 0.5), diameter: 1.0, brightness: 1.0),
+                        .generate(location: .init(x: 2.0, y: 2.0), diameter: 2.0, brightness: 2.0),
+                        .generate(location: .init(x: 3.5, y: 3.5), diameter: 3.0, brightness: 3.0)
+                    ]
+                ),
+                expectation: .init(
+                    result: []
+                )
+            ),
+            /// The case with 4 points
+            (
+                /// Confirm that the start of the curve is the second point and the end of the curve is the second-to-last point,
+                /// since the first and last points are used for the Bezier curve handles.
+                condition: .init(
+                    shouldIncludeEndPoint: true,
+                    points: [
+                        .generate(location: .init(x: 0.5, y: 0.5), diameter: 1.0, brightness: 1.0),
+                        .generate(location: .init(x: 2.0, y: 2.0), diameter: 2.0, brightness: 2.0),
+                        .generate(location: .init(x: 3.5, y: 3.5), diameter: 3.0, brightness: 3.0),
+                        .generate(location: .init(x: 5.0, y: 5.0), diameter: 4.0, brightness: 4.0)
+                    ]
+                ),
+                expectation: .init(
+                    result: [
+                        .generate(location: .init(x: 2.0, y: 2.0), diameter: 2.0, brightness: 2.0),
+                        .generate(location: .init(x: 2.75, y: 2.75), diameter: 2.5, brightness: 2.5),
+                        .generate(location: .init(x: 3.5, y: 3.5), diameter: 3.0, brightness: 3.0)
+                    ]
+                )
+            ),
+            (
+                /// Confirm that the last point is not added
+                condition: .init(
+                    shouldIncludeEndPoint: false,
+                    points: [
+                        .generate(location: .init(x: 0.5, y: 0.5), diameter: 1.0, brightness: 1.0),
+                        .generate(location: .init(x: 2.0, y: 2.0), diameter: 2.0, brightness: 2.0),
+                        .generate(location: .init(x: 3.5, y: 3.5), diameter: 3.0, brightness: 3.0),
+                        .generate(location: .init(x: 5.0, y: 5.0), diameter: 4.0, brightness: 4.0)
+                    ]
+                ),
+                expectation: .init(
+                    result: [
+                        .generate(location: .init(x: 2.0, y: 2.0), diameter: 2.0, brightness: 2.0),
+                        .generate(location: .init(x: 2.75, y: 2.75), diameter: 2.5, brightness: 2.5)
+                    ]
+                )
+            ),
+
+            /// The case with more than 4 points
+            (
+                /// Confirm that the start of the curve is the second point and the end of the curve is the second-to-last point,
+                /// as the first and last points are used for the Bezier curve handles, even when there are more than four points.”
+                condition: .init(
+                    shouldIncludeEndPoint: true,
+                    points: [
+                        .generate(location: .init(x: 0.5, y: 0.5), diameter: 1.0, brightness: 1.0),
+                        .generate(location: .init(x: 2.0, y: 2.0), diameter: 2.0, brightness: 2.0),
+                        .generate(location: .init(x: 3.5, y: 3.5), diameter: 3.0, brightness: 3.0),
+                        .generate(location: .init(x: 5.0, y: 5.0), diameter: 4.0, brightness: 4.0),
+                        .generate(location: .init(x: 6.5, y: 6.5), diameter: 5.0, brightness: 5.0),
+                        .generate(location: .init(x: 8.0, y: 8.0), diameter: 6.0, brightness: 6.0)
+                    ]
+                ),
+                expectation: .init(
+                    result: [
+                        .generate(location: .init(x: 2.0, y: 2.0), diameter: 2.0, brightness: 2.0),
+                        .generate(location: .init(x: 2.75, y: 2.75), diameter: 2.5, brightness: 2.5),
+                        .generate(location: .init(x: 3.5, y: 3.5), diameter: 3.0, brightness: 3.0),
+                        .generate(location: .init(x: 4.25, y: 4.25), diameter: 3.5, brightness: 3.5),
+                        .generate(location: .init(x: 5.0, y: 5.0), diameter: 4.0, brightness: 4.0),
+                        .generate(location: .init(x: 5.75, y: 5.75), diameter: 4.5, brightness: 4.5),
+                        .generate(location: .init(x: 6.5, y: 6.5), diameter: 5.0, brightness: 5.0)
+                    ]
+                )
+            ),
+            (
+                /// Confirm that the last point is not added
+                condition: .init(
+                    shouldIncludeEndPoint: false,
+                    points: [
+                        .generate(location: .init(x: 0.5, y: 0.5), diameter: 1.0, brightness: 1.0),
+                        .generate(location: .init(x: 2.0, y: 2.0), diameter: 2.0, brightness: 2.0),
+                        .generate(location: .init(x: 3.5, y: 3.5), diameter: 3.0, brightness: 3.0),
+                        .generate(location: .init(x: 5.0, y: 5.0), diameter: 4.0, brightness: 4.0),
+                        .generate(location: .init(x: 6.5, y: 6.5), diameter: 5.0, brightness: 5.0),
+                        .generate(location: .init(x: 8.0, y: 8.0), diameter: 6.0, brightness: 6.0)
+                    ]
+                ),
+                expectation: .init(
+                    result: [
+                        .generate(location: .init(x: 2.0, y: 2.0), diameter: 2.0, brightness: 2.0),
+                        .generate(location: .init(x: 2.75, y: 2.75), diameter: 2.5, brightness: 2.5),
+                        .generate(location: .init(x: 3.5, y: 3.5), diameter: 3.0, brightness: 3.0),
+                        .generate(location: .init(x: 4.25, y: 4.25), diameter: 3.5, brightness: 3.5),
+                        .generate(location: .init(x: 5.0, y: 5.0), diameter: 4.0, brightness: 4.0),
+                        .generate(location: .init(x: 5.75, y: 5.75), diameter: 4.5, brightness: 4.5)
+                    ]
+                )
+            )
+        ]
+
+        for testCase in testCases {
+            let condition = testCase.condition
+            let expectation = testCase.expectation
+
+            let iterator = CanvasGrayscaleCurveIterator()
+            iterator.append(condition.points)
+
+            let result = CanvasGrayscaleDotPoint.makeCurveIntermediatePoints(
+                from: iterator,
+                shouldIncludeEndPoint: condition.shouldIncludeEndPoint
+            )
+            XCTAssertEqual(result, expectation.result)
+        }
+    }
+
+    /// Confirm that the first curve array can be created
+    func testMakeCurveFirstPoints() {
+        struct Condition {
+            let points: [CanvasGrayscaleDotPoint]
+        }
+        struct Expectation {
+            let result: [CanvasGrayscaleDotPoint]
+        }
+
+        let testCases: [(condition: Condition, expectation: Expectation)] = [
+            (
+                /// The case with fewer than 3 points
+                condition: .init(
+                    points: [
+                        .generate(location: .init(x: 0.5, y: 0.5), diameter: 1.0, brightness: 1.0),
+                        .generate(location: .init(x: 2.0, y: 2.0), diameter: 2.0, brightness: 2.0)
+                    ]
+                ),
+                expectation: .init(
+                    result: []
+                )
+            ),
+            (
+                /// The start of the curve connects to the first point, and the end of the curve connects to the second point. The last point is used to create the Bezier curve handles.
+                /// The first curve does not include the last point to connect with the next curve.
+                condition: .init(
+                    points: [
+                        .generate(location: .init(x: 0.5, y: 0.5), diameter: 1.0, brightness: 1.0),
+                        .generate(location: .init(x: 2.0, y: 2.0), diameter: 2.0, brightness: 2.0),
+                        .generate(location: .init(x: 3.5, y: 3.5), diameter: 3.0, brightness: 3.0)
+                    ]
+                ),
+                expectation: .init(
+                    result: [
+                        .generate(location: .init(x: 0.5, y: 0.5), diameter: 1.0, brightness: 1.0),
+                        .generate(location: .init(x: 1.25, y: 1.25), diameter: 1.5, brightness: 1.5)
+                        /// `(x: 2.0, y: 2.0)` is not included
+                    ]
+                )
+            )
+        ]
+
+        for testCase in testCases {
+            let condition = testCase.condition
+            let expectation = testCase.expectation
+
+            let iterator = CanvasGrayscaleCurveIterator()
+            iterator.append(condition.points)
+
+            let result = CanvasGrayscaleDotPoint.makeCurveFirstPoints(
+                from: iterator
+            )
+            XCTAssertEqual(result, expectation.result)
+        }
+    }
+
+    /// Confirm that the last curve array can be created
+    func testMakeCurveLastPoints() {
+        struct Condition {
+            let points: [CanvasGrayscaleDotPoint]
+        }
+        struct Expectation {
+            let result: [CanvasGrayscaleDotPoint]
+        }
+
+        let testCases: [(condition: Condition, expectation: Expectation)] = [
+            (
+                /// The case with fewer than 3 points
+                condition: .init(
+                    points: [
+                        .generate(location: .init(x: 0.5, y: 0.5), diameter: 1.0, brightness: 1.0),
+                        .generate(location: .init(x: 2.0, y: 2.0), diameter: 2.0, brightness: 2.0)
+                    ]
+                ),
+                expectation: .init(
+                    result: []
+                )
+            ),
+            (
+                /// The start of the curve connects to the second point, and the end of the curve connects to the last point. The first point is used to create the Bezier curve handles. 
+                /// The last curve includes the last point since it does not connect with the next curve.
+                condition: .init(
+                    points: [
+                        .generate(location: .init(x: 0.5, y: 0.5), diameter: 1.0, brightness: 1.0),
+                        .generate(location: .init(x: 2.0, y: 2.0), diameter: 2.0, brightness: 2.0),
+                        .generate(location: .init(x: 3.5, y: 3.5), diameter: 3.0, brightness: 3.0)
+                    ]
+                ),
+                expectation: .init(
+                    result: [
+                        .generate(location: .init(x: 2.0, y: 2.0), diameter: 2.0, brightness: 2.0),
+                        .generate(location: .init(x: 2.75, y: 2.75), diameter: 2.5, brightness: 2.5),
+                        .generate(location: .init(x: 3.5, y: 3.5), diameter: 3.0, brightness: 3.0)
+                    ]
+                )
+            )
+        ]
+
+        for testCase in testCases {
+            let condition = testCase.condition
+            let expectation = testCase.expectation
+
+            let iterator = CanvasGrayscaleCurveIterator()
+            iterator.append(condition.points)
+
+            let result = CanvasGrayscaleDotPoint.makeCurveLastPoints(
+                from: iterator
+            )
+            XCTAssertEqual(result, expectation.result)
+        }
+    }
+
+    func testInterpolateAndMatchArraySizes() {
+        struct Condition {
+            let shouldIncludeEndPoint: Bool
+            let targetPoints: [CGPoint]
+            let startPoint: CanvasGrayscaleDotPoint
+            let endPoint: CanvasGrayscaleDotPoint
+        }
+        struct Expectation {
+            let result: [CanvasGrayscaleDotPoint]
+        }
+
+        let testCases: [(condition: Condition, expectation: Expectation)] = [
+            (
+                // Interpolate to match `targetPoints`, including `endPoint` values
+                condition: .init(
+                    shouldIncludeEndPoint: true,
+                    targetPoints: [
+                        .init(x: 0, y: 0),
+                        .init(x: 10, y: 10),
+                        .init(x: 20, y: 20),
+                        .init(x: 30, y: 30),
+                        .init(x: 40, y: 40)
+                    ],
+                    startPoint: .generate(location: .init(x: 9999, y: 9999), diameter: 0, brightness: 0),
+                    endPoint: .generate(location: .init(x: 9999, y: 9999), diameter: 1.0, brightness: 1.0)
+                ),
+                // `location` of `startPoint` and `endPoint` are ignored,
+                // and `location` of `targetPoints` is used instead.
+                expectation: .init(result: [
+                    .generate(location: .init(x: 0, y: 0), diameter: 0.0, brightness: 0.0),
+                    .generate(location: .init(x: 10, y: 10), diameter: 0.25, brightness: 0.25),
+                    .generate(location: .init(x: 20, y: 20), diameter: 0.5, brightness: 0.5),
+                    .generate(location: .init(x: 30, y: 30), diameter: 0.75, brightness: 0.75),
+                    .generate(location: .init(x: 40, y: 40), diameter: 1.0, brightness: 1.0)
+                ])
+            ),
+            (
+                // Interpolate to match `targetPoints`, excluding `endPoint` values
+                condition: .init(
+                    shouldIncludeEndPoint: false,
+                    targetPoints: [
+                        .init(x: 0, y: 0),
+                        .init(x: 10, y: 10),
+                        .init(x: 20, y: 20),
+                        .init(x: 30, y: 30)
+                    ],
+                    startPoint: .generate( location: .init(x: 9999, y: 9999), diameter: 0, brightness: 0),
+                    endPoint: .generate( location: .init(x: 9999, y: 9999), diameter: 1.0, brightness: 1.0)
+                ),
+                // `location` of `startPoint` and `endPoint` are ignored,
+                // and `location` of `targetPoints` is used instead.
+                expectation: .init(result: [
+                    .generate(location: .init(x: 0, y: 0), diameter: 0.0, brightness: 0.0),
+                    .generate(location: .init(x: 10, y: 10), diameter: 0.25, brightness: 0.25),
+                    .generate(location: .init(x: 20, y: 20), diameter: 0.5, brightness: 0.5),
+                    .generate(location: .init(x: 30, y: 30), diameter: 0.75, brightness: 0.75
+                    )
+                ])
+            )
+        ]
+
+        for testCase in testCases {
+            let condition = testCase.condition
+            let expectation = testCase.expectation
+
+            let result = CanvasGrayscaleDotPoint.interpolateToMatchPointCount(
+                targetPoints: condition.targetPoints,
+                interpolationStart: condition.startPoint,
+                interpolationEnd: condition.endPoint,
+                shouldIncludeEndPoint: condition.shouldIncludeEndPoint
+            )
+            XCTAssertEqual(result, expectation.result)
+        }
+    }
+
+}
