f8823eea38bb3e68f437ac01340a176cbb048302
EisukeKusachi <eisukekusachi@gmail.com>
2024-11-09 18:46:32 +0900

    Remove an unnecessary space

--- SimpleApplePencilDrawing/Extensions/UIColorExtensions.swift
+++ SimpleApplePencilDrawing/Extensions/UIColorExtensions.swift
@@ -44,7 +44,7 @@ extension UIColor {
         self.init(
             red: (CGFloat(red) / 255.0),
             green: (CGFloat(green) / 255.0),
-            blue: (CGFloat(blue) / 255.0), 
+            blue: (CGFloat(blue) / 255.0),
             alpha: (CGFloat(alpha) / 255.0)
         )
     }
