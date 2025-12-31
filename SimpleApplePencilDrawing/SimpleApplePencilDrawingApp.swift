eac9f80621874b372539c2d7da14687bcc88577e
Eisuke Kusachi <eisukekusachi@gmail.com>
2025-12-31 11:09:48 +0900

    Remove `AppDelegate`

---
+++ SimpleApplePencilDrawing/SimpleApplePencilDrawingApp.swift
@@ -0,0 +1,20 @@
+//
+//  SimpleApplePencilDrawingApp.swift
+//  SimpleApplePencilDrawing
+//
+//  Created by Eisuke Kusachi on 2025/12/31.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct SimpleApplePencilDrawingApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .ignoresSafeArea()
+        }
+    }
+}
